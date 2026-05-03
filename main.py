9f48d22fc3e731df1d4e3f27a4e1cc11ed6d7363
Tharun J <tharunkumar.vg@gmail.com>
2026-05-04 05:08:14 +0530

    Add files via upload
    Added register and login API

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+# Temporary database (just for now)
+users = []
+
+# User model
+class User(BaseModel):
+    username: str
+    password: str
 
 @app.get("/")
 def read_root():
     return {"message": "Secure Auth API is running"}
+
+# Register API
+@app.post("/register")
+def register(user: User):
+    users.append(user)
+    return {"message": "User registered successfully"}
+
+# Login API
+@app.post("/login")
+def login(user: User):
+    for u in users:
+        if u.username == user.username and u.password == user.password:
+            return {"message": "Login successful"}
+    return {"message": "Invalid credentials"}
